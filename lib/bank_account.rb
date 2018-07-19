adac0460009395117bde496fa6b3ef6ff566feb7
github username <github email address>
2018-07-19 15:12:53 -0500

    updated valid? logic

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -5,17 +5,10 @@ class BankAccount
   attr_accessor :balance, :status
   attr_reader :name
 
-  @@all = []
-
   def initialize(name)
     @name = name
     @balance = 1000
     @status = "open"
-    @@all << self
-  end
-
-  def self.all
-    @@all
   end
 
   def deposit(positive_deposit_amount)
@@ -23,7 +16,7 @@ class BankAccount
   end
 
   def display_balance
-    puts @balance
+    "Your balance is $#{self.balance}."
   end
 
   def valid?
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,29 +1,19 @@
 class Transfer
 
+  attr_accessor :status
   attr_reader :sender, :receiver
 
   def initialize(sender,
     receiver,
-    transfer_amount)
+    amount)
     @sender = sender
     @receiver = receiver
-    @transfer_amount = transfer_amount
+    @amount = amount
     @status = "pending"
   end
 
   def valid?
-    sender_valid = false
-    receiver_valid = false
-
-    BankAccount.all.each do |bank_account_instance|
-      if bank_account_instance == @sender && bank_account_instance.valid?
-        sender_valid = true
-      elsif bank_account_instance == @receiver && bank_account_instance.valid?
-        receiver_valid = true
-      end
-    end
-
-      sender_valid && receiver_valid
+    @sender.valid? && @receiver.valid?
   end
 
   def execute_transaction
