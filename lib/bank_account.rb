4c56bdd0c0e8914db65e595c8f00ef65660b1d43
github username <github email address>
2018-07-19 14:51:59 -0500

    partially completed

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,37 @@
+require "pry"
+
 class BankAccount
 
+  attr_accessor :balance, :status
+  attr_reader :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def deposit(positive_deposit_amount)
+    @balance += positive_deposit_amount
+  end
+
+  def display_balance
+    puts @balance
+  end
+
+  def valid?
+    @status == "open" && @balance > 0
+  end
+
+  def close_account
+    @status = "closed"
+  end
 end
+# binding.pry
